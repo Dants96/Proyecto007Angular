9ac34599a629adb3dff228884f232e50147785ad
Dants96 <danieltutistar@gmail.com>
2021-10-26 21:15:36 -0500

    ejercicio terminado

---
+++ src/app/selector-numerico/selector-numerico.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selector-numerico',
+  templateUrl: './selector-numerico.component.html',
+  styleUrls: ['./selector-numerico.component.css']
+})
+export class SelectorNumericoComponent implements OnInit {
+  @Input() minimo!: string | number;
+  @Input() maximo!: string | number;
+  actual!: number;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.maximo = Number(this.maximo);
+    this.minimo = Number(this.minimo);
+    console.log("maxio: ", this.maximo, " minimo: ", this.minimo);
+    this.actual = this.minimo;
+  }
+
+  incrementar(): void{
+    if(this.actual < this.maximo) this.actual ++;
+  }
+
+  decrementar(): void{
+    if(this.actual > this.minimo) this.actual --;
+  }
+
+  fijar(v:number): void{
+    if(v>=this.minimo && v<=this.maximo){
+      this.actual = v;
+    }
+  }
+
+}
